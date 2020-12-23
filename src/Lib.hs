78d6e35ee8762a21c86d21c41515bb4b5efc170f
Andrew Hao <andrewhao@gmail.com>
2020-12-22 22:11:16 -0800

    Adding first draft CSV parser

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,6 +1,42 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Lib
-    ( someFunc
+    ( parseCsv
     ) where
 
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+import           Control.Applicative        ()
+import qualified Data.ByteString.Lazy.Char8 as BL
+import           Data.Csv                   (FromNamedRecord (..),
+                                             HasHeader (NoHeader), decode,
+                                             decodeByName, (.:))
+import qualified Data.Vector                as V
+
+data UsageRecord = UsageRecord {
+   usageType :: String
+,  date      :: String
+,  startTime :: String
+,  endTime   :: String
+,  usage     :: Float
+,  units     :: String
+} deriving (Show)
+
+instance FromNamedRecord UsageRecord where
+    parseNamedRecord r =
+        UsageRecord
+            <$> r .: "TYPE"
+            <*> r .: "DATE"
+            <*> r .: "START TIME"
+            <*> r .: "END TIME"
+            <*> r .: "USAGE"
+            <*> r .: "UNITS"
+
+
+
+parseCsv :: IO ()
+parseCsv = do
+    csvData <- BL.readFile "sample1.csv"
+    case decodeByName csvData of
+        Left err -> putStrLn err
+        Right (h, v) ->
+            V.forM_ v $ \ p ->
+                putStrLn $ usageType p ++ date p ++ startTime p ++ endTime p ++ show (usage p) ++ units p
