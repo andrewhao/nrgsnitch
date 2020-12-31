58dddfbeb5c425b0a2c5e2cdbb084d28ac709b04
Andrew Hao <andrewhao@gmail.com>
2020-12-31 00:48:22 -0800

    WIP - introducing date parsing and filtering

--- src/Lib.hs
+++ src/Lib.hs
@@ -7,9 +7,12 @@ module Lib
 import           Control.Applicative        ()
 import qualified Data.ByteString.Lazy.Char8 as BL
 import           Data.Csv                   (FromNamedRecord (..),
-                                             HasHeader (NoHeader), decode,
-                                             decodeByName, (.:))
+                                             HasHeader (NoHeader), Header,
+                                             decode, decodeByName, header, (.:))
+import           Data.Either                (fromRight)
+import           Data.Time
 import qualified Data.Vector                as V
+import  qualified Data.Maybe as MB
 
 data UsageRecord = UsageRecord {
    usageType :: String
@@ -18,6 +21,7 @@ data UsageRecord = UsageRecord {
 ,  endTime   :: String
 ,  usage     :: Float
 ,  units     :: String
+,  cost      :: String
 } deriving (Show)
 
 instance FromNamedRecord UsageRecord where
@@ -29,14 +33,51 @@ instance FromNamedRecord UsageRecord where
             <*> r .: "END TIME"
             <*> r .: "USAGE"
             <*> r .: "UNITS"
+            <*> r .: "COST"
+
+getCsvDataFromCsv :: Either String (Header, V.Vector UsageRecord) -> (Header, V.Vector UsageRecord)
+getCsvDataFromCsv = fromRight (header [], V.empty)
 
 
+getTotalUsages :: V.Vector UsageRecord -> Integer
+getTotalUsages usages = 0
+
+parseTimeString :: String -> Maybe TimeOfDay
+parseTimeString =
+    parseTimeM True defaultTimeLocale "%H:%M"
+
+parseDateString :: String -> Day
+parseDateString =
+    let date = parseTimeM True defaultTimeLocale "%F" :: Maybe Day
+
+    MB.fromMaybe (fromGregorian 1970 1 1) date
+
+isElectricChargingUsage :: UsageRecord -> Bool
+isElectricChargingUsage record =
+    True
+
+printRecord :: UsageRecord -> IO ()
+printRecord p =
+   putStrLn $ usageType p ++ date p ++ startTime p ++ endTime p ++ show (usage p) ++ units p ++ cost p
 
 parseCsv :: String -> IO ()
 parseCsv fileName = do
     csvData <- BL.readFile fileName
-    case decodeByName csvData of
-        Left err -> putStrLn err
-        Right (h, v) ->
-            V.forM_ v $ \ p ->
-                putStrLn $ usageType p ++ date p ++ startTime p ++ endTime p ++ show (usage p) ++ units p
+    let decoded = decodeByName csvData
+    let (_, records) = getCsvDataFromCsv decoded
+
+    putStrLn "Filtered"
+
+    let filteredRecords = V.filter isElectricChargingUsage records
+    V.forM_ filteredRecords printRecord
+
+    putStrLn "Date"
+
+    V.forM_ filteredRecords (\r -> putStrLn parseDateString (date r))
+
+
+
+    -- case decoded of
+    --     Left err -> putStrLn err
+    --     Right (h, v) ->
+    --         V.forM_ v printRecord
